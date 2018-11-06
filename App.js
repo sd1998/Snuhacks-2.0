f763f39178298bf45f672d03244bf0ecf030b6a7
sd1998 <shashvat51@gmail.com>
2018-11-06 20:56:02 +0530

    Attendance summary fetch fixed

--- App.js
+++ App.js
@@ -8,8 +8,6 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
-const cheerio = require('react-native-cheerio');
-const request = require('request')
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -22,7 +20,8 @@ type Props = {};
 export default class App extends Component<Props> {
 
   componentDidMount() {
-    data = {
+
+    /*data = {
       "login_user_name": "sk261",
       "login_password": "Dnisg@12344"
     }
@@ -31,14 +30,33 @@ export default class App extends Component<Props> {
         console.log(err)
       }
       console.log(response)
-      })
-      //fetch("https://markattendance.webapps.snu.edu.in/public/application/login/loginAuthSubmit", //{"credentials":"include","headers":{"accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8","accept-language":"en-US,en;q=0.9","cache-control":"max-age=0","content-type":"application/x-www-form-urlencoded","upgrade-insecure-requests":"1"},"referrer":"https://markattendance.webapps.snu.edu.in/public/application/login/login","referrerPolicy":"no-referrer-when-downgrade","body":"login_user_name=sk261&login_password=Dnisg%4012344","method":"POST","mod//e":"cors"}).then((response) => {
-    //    console.log("AAAAA")
-    //    console.log(response)
-
-    //    }).catch((err) => {
-    //      console.log(err)
-    //      })
+      })*/
+      fetch("https://markattendance.webapps.snu.edu.in/public/application/login/loginAuthSubmit",
+      {"method": "POST","credentials":"same-origin",
+      "headers":{"accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+      "accept-language":"en-US,en;q=0.9",
+      "cache-control":"max-age=0",
+      "content-type":"application/x-www-form-urlencoded",
+      "upgrade-insecure-requests":"1"},
+      "referrer":"https://markattendance.webapps.snu.edu.in/public/application/login/login",
+      "referrerPolicy":"no-referrer-when-downgrade",
+      "body":"login_user_name=sk261&login_password=Dnisg%4012344","method":"POST","mod//e":"cors"}).then((response) => {
+        console.log("AAAAA")
+        console.log(response)
+        fetch("https://markattendance.webapps.snu.edu.in/public/application/index/summary",
+         {"credentials":"include",
+         "headers":{"accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8","accept-language":"en-US,en;q=0.9","cache-control":"max-age=0","upgrade-insecure-requests":"1"},
+         "referrerPolicy":"no-referrer-when-downgrade",
+         "body":null,
+         "method":"GET",
+         "mode":"cors"}).then((response) => {
+           console.log(response._bodyText)
+           }).catch((err) => {
+             console.log(err)
+             });
+        }).catch((err) => {
+          console.log(err)
+        })
     /*return fetch("https://markattendance.webapps.snu.edu.in/public/application/login/login").then((response) => {
       const $ = cheerio.load(response._bodyText)
       $(".panel-body").each(function(index,articles){
