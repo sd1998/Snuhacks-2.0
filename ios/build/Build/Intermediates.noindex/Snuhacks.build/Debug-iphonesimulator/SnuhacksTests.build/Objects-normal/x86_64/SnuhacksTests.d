abcdb1677805cdfe8056bbbf9408233381d3e3f7
sd1998 <shashvat51@gmail.com>
2018-11-18 11:34:44 +0530

    Build files removed

---
+++ ios/build/Build/Intermediates.noindex/Snuhacks.build/Debug-iphonesimulator/SnuhacksTests.build/Objects-normal/x86_64/SnuhacksTests.d
@@ -0,0 +1,13 @@
+dependencies: \
+  /Users/mayanksaxena/Desktop/Snuhacks-2.0/ios/SnuhacksTests/SnuhacksTests.m \
+  /Users/mayanksaxena/Desktop/Snuhacks-2.0/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTLog.h \
+  /Users/mayanksaxena/Desktop/Snuhacks-2.0/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTAssert.h \
+  /Users/mayanksaxena/Desktop/Snuhacks-2.0/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTDefines.h \
+  /Users/mayanksaxena/Desktop/Snuhacks-2.0/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTUtils.h \
+  /Users/mayanksaxena/Desktop/Snuhacks-2.0/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTRootView.h \
+  /Users/mayanksaxena/Desktop/Snuhacks-2.0/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTBridge.h \
+  /Users/mayanksaxena/Desktop/Snuhacks-2.0/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTBridgeDelegate.h \
+  /Users/mayanksaxena/Desktop/Snuhacks-2.0/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTJavaScriptLoader.h \
+  /Users/mayanksaxena/Desktop/Snuhacks-2.0/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTBridgeModule.h \
+  /Users/mayanksaxena/Desktop/Snuhacks-2.0/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTFrameUpdate.h \
+  /Users/mayanksaxena/Desktop/Snuhacks-2.0/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTInvalidating.h
